e3a0482d1e5ef89d8ec15c2886af6c518d5c60f3
pevensey <yuliantopambudi45@gmail.com>
2020-02-13 08:41:40 +0700

    cleanup

--- src/main/java/com/rest/api/controller/AkunController.java
+++ src/main/java/com/rest/api/controller/AkunController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.management.Query;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -20,30 +19,21 @@ public class AkunController {
     private AkunRepository akunRepo;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-
     @RequestMapping("/hello")
     public String hello(){
         return "Hello Akun  ";
     }
+
     @GetMapping("/")
     public List<Akun> getAll(){
         return akunRepo.findAll();
     }
+
     @PostMapping("/tambahakun")
     public Akun tambahAkun(@Valid @RequestBody Akun akun) {
         return akunRepo.save(akun);
     }
 
-
-    @PostMapping("/tambah")
-    public Akun tambahManual() {
-
-        Akun akun = new Akun();
-        akun.setUsername("cobacoba.com");
-        akun.setPassword("123456");
-
-        return akunRepo.save(akun);
-    }
     public AkunController(AkunRepository akunRepo,
                           BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.akunRepo = akunRepo;
@@ -63,59 +53,6 @@ public class AkunController {
             return new ResponseEntity<>(HttpStatus.CREATED);
         }
     }
-//    @PostMapping("/")
-//    public Akun login(@Valid @RequestBody Akun akun) {
-//        return akunRepo.save(akun);
-//    }
-
-
-    //    }@PostMapping("/")
-//    @ResponseStatus(value = HttpStatus.OK)
-//    public  ResponseEntity<Akun>  cobalogin(String nm, String ps) {
-//        //return akunRepo.findByemail(nm);
-//
-//        List<Akun> a = akunRepo.findByemail(nm);
-//        if(a==null){
-//            System.out.println(ResponseEntity.ok().body(a));
-//            return ResponseEntity.notFound().build();
-//            //return ResponseEntity.ok().body(nm);
-//        }else {
-//            //System.out.println(ResponseEntity.notFound());
-//            return ResponseEntity.ok().body(a);
-//        }
-//    @PostMapping("/")
-////@ResponseStatus(value = HttpStatus.OK)
-//    public ResponseEntity<Akun> login(@Valid @RequestBody Akun akun) {
-//
-//
-//        String email = akun.getUsername();
-//        String pass = akun.getPassword();
-//
-//        String pass_benar = akunRepo.findKataSandi(pass);
-//        String email_benar = akunRepo.findEmail(email);
-//
-//        ResponseEntity<Akun> a = null;
-//
-//        if (email.equals(email_benar)) {
-//            if (pass.isEmpty())  //pass!=pass_benar
-//                a = ResponseEntity.notFound().build();
-//
-//            if (pass.equals(pass_benar))  //pass==pass_benar
-//                a = ResponseEntity.ok().body(akun);
-//
-//        }else{
-//            a=ResponseEntity.badRequest().build();
-//        }
-//        System.out.println(email);
-//        System.out.println(email_benar);
-////        System.out.println(pass);
-////        System.out.println(pass_benar);
-////        System.out.println(a);
-////
-//        return a;
-//    }
-
-
 }
 
 
--- src/main/java/com/rest/api/repository/AkunRepository.java
+++ src/main/java/com/rest/api/repository/AkunRepository.java
@@ -8,17 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AkunRepository extends JpaRepository<Akun, Long> {
-
-    @Query("SELECT password FROM Akun where password=pass")
-    String find_password(String pass);
-
-    @Query("SELECT t.password FROM Akun t where t.password = :password")
-    String findKataSandi(@Param("password") String pass);
-//    @Query("SELECT username from AKUN where username=username")
-
-
-//    @Query("SELECT t.email FROM Akun t where t.email = :email")
-//    String findEmail(@Param("email") String email);
-
     Akun findByUsername(String username);
 }
